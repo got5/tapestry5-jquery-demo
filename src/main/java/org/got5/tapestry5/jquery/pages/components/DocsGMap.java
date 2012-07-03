7902c97089f70cbd9f877e1ad7290cd7b6ef90bd
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2012-07-03 14:27:28 +0200

    Add Google Map Demo

---
+++ src/main/java/org/got5/tapestry5/jquery/pages/components/DocsGMap.java
@@ -0,0 +1,10 @@
+package org.got5.tapestry5.jquery.pages.components;
+
+import org.apache.tapestry5.json.JSONObject;
+
+public class DocsGMap {
+	public JSONObject getOptions(){
+		JSONObject json = new JSONObject();
+		return json;
+	}
+}
