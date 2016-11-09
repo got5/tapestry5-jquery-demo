61edeac0ac8b6c09b86d6ae565ab0dbf151e0725
ffacon <francois.facon@gmail.com>
2016-11-09 17:18:26 +0100

    Change Samples to follow new jQuery ui file naming convension

--- src/main/java/org/got5/tapestry5/jquery/pages/mecanism/DocsJavaScript.java
+++ src/main/java/org/got5/tapestry5/jquery/pages/mecanism/DocsJavaScript.java
@@ -2,7 +2,7 @@ package org.got5.tapestry5.jquery.pages.mecanism;
 
 import org.got5.tapestry5.jquery.ImportJQueryUI;
 
-@ImportJQueryUI("jquery.ui.sortable")
+@ImportJQueryUI("widgets/sortable")
 public class DocsJavaScript
 {
 }
